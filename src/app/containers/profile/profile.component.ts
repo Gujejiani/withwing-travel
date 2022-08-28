3a24124c56cd4720e101dd1354f1cece6522ba6b
kakha <kakha.gujejiani@singular.uk>
2022-08-28 22:19:00 +0400

    reactive form validations added

---
+++ src/app/containers/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Service/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private userService: UserService) { }
+  user: any ={}
+  ngOnInit(): void {
+    this.user=  this.userService.getUser()
+
+    console.log(this.user)
+  }
+
+}
